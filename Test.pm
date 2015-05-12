90a5ad4328adfe5dd868bf7f794456b2df25c319
Jan Gehring <jan.gehring@dm.de>
2015-05-12 11:47:45 +0200

    fixed some things

--- Test.pm
+++ Test.pm
@@ -17,7 +17,7 @@ sub test {
   $param->{port}     = ( ref $param->{port} eq "CODE" ? $param->{port}->() : $param->{port} );
   $param->{host}     = ( ref $param->{host} eq "CODE" ? $param->{host}->() : $param->{host} );
 
-  for my $loc ( @$param->{location} ) {
+  for my $loc ( @{ $param->{location} } ) {
     my $res = $self->ua->get("http://$param->{host}:$param->{port}$loc");
     if($res->code != 200) {
       die "Error testing url: http://$param->{host}:$param->{port}$loc";
--- __module__.pm
+++ __module__.pm
@@ -3,12 +3,15 @@ package Apptest;
 
 use common::sense;
 use Rex -base;
+use Apptest::Test;
 
 task "test", group => "servers", sub {
   my $param = shift;
 
   $param->{test_object} ||= "Apptest::Test";
+
   my $test = $param->{test_object}->new();
+
   $test->test($param);
 };
 
