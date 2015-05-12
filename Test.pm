1d0080959ca1a9fcb8c17d092576900f02d2620d
Jan Gehring <jan.gehring@dm.de>
2015-05-12 09:41:15 +0200

    test objekt

---
+++ Test.pm
@@ -0,0 +1,28 @@
+package Apptest::Test;
+
+use Moose;
+use Apptest::UserAgent;
+
+has ua => (
+  is      => 'ro',
+  default => sub {
+    return Apptest::UserAgent->new;
+  }
+);
+
+sub test {
+  my ($self, $param) = @_;
+
+  $param->{location} = ( ref $param->{location} eq "ARRAY" ? $param->{location} : [ $param->{location} ] );
+  $param->{port}     = ( ref $param->{port} eq "CODE" ? $param->{port}->() : $param->{port} );
+  $param->{host}     = ( ref $param->{host} eq "CODE" ? $param->{host}->() : $param->{host} );
+
+  for my $loc ( @$param->{location} ) {
+    my $res = $self->ua->get("http://$param->{host}:$param->{port}$loc");
+    if($res->code != 200) {
+      die "Error testing url: http://$param->{host}:$param->{port}$loc";
+    }
+  }
+}
+
+1;
--- __module__.pm
+++ __module__.pm
@@ -7,18 +7,9 @@ use Rex -base;
 task "test", group => "servers", sub {
   my $param = shift;
 
-  my $ua = Apptest::UserAgent->new;
-  $param->{location} = ( ref $param->{location} eq "ARRAY" ? $param->{location} : [ $param->{location} ] );
-  $param->{port}     = ( ref $param->{port} eq "CODE" ? $param->{port}->() : $param->{port} );
-  $param->{host}     = ( ref $param->{host} eq "CODE" ? $param->{host}->() : $param->{host} );
-
-  for my $loc ( @$param->{location} ) {
-    my $res = $ua->get("http://$param->{host}:$param->{port}$loc");
-    if($res->code != 200) {
-      die "Error testing url: http://$param->{host}:$param->{port}$loc";
-    }
-  }
-
+  $param->{test_object} ||= "Apptest::Test";
+  my $test = $param->{test_object}->new();
+  $test->test($param);
 };
 
 1;
