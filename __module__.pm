68f044ada896d4b1ee8a735d153b0c32058e7e3f
Jan Gehring <jan.gehring@dm.de>
2015-07-01 08:01:40 +0200

    reverted change in master

--- __module__.pm
+++ __module__.pm
@@ -17,7 +17,7 @@ task "test", group => "servers", sub {
   $param->{test_object} ||= "Apptest::Test";
 
   my $test = $param->{test_object}->new(project => $param->{project});
-  $test->port($param->{project}->application->get_inactive()->port);
+  $test->port($param->{project}->get_inactive_tomcat()->port);
   
 
   $test->test($param);
